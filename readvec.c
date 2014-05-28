d48c57920a30ce2dc350997ec1b65eba10868b13
Joakim Söderberg <joakim.soderberg@gmail.com>
2014-05-28 21:41:19 +0200

    Enable merging vec files.

--- readvec.c
+++ readvec.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
 
 typedef struct CvVecFile
 {
@@ -13,6 +14,23 @@ typedef struct CvVecFile
 	short* vector;
 } CvVecFile;
 
+int write_vec_header(FILE *f, int count, int vecsize, short min, short max)
+{
+	if (fseek(f, 0, SEEK_SET))
+	{
+		fprintf(stderr, "Failed to seek in file\n");
+		return -1;
+	}
+
+	fwrite(&count, sizeof(count), 1, f);
+	fwrite(&vecsize, sizeof(vecsize), 1, f);
+	fwrite(&min, sizeof(min), 1, f);
+	fwrite(&max, sizeof(max), 1, f);
+
+	//fseek(f, 0, SEEK_END);
+	return 0;
+}
+
 int main(int argc, char **argv)
 {
 	int ret = 0;
@@ -20,9 +38,13 @@ int main(int argc, char **argv)
 	CvVecFile *vecs = NULL;
 	CvVecFile *vec = NULL;
 	CvVecFile mergevec;
-	short width;
-	short height;
+	int vecsize = 0;
+	short min;
+	short max;
 	int i;
+	FILE *output = NULL;
+	short *samples = NULL;
+	int vec_index = 0;
 	char *merge = NULL;
 	memset(&mergevec, 0, sizeof(mergevec));
 
@@ -43,6 +65,7 @@ int main(int argc, char **argv)
 				return -1;
 			}
 
+			i++;
 			merge = argv[i];
 		}
 		else
@@ -57,46 +80,119 @@ int main(int argc, char **argv)
 		return -1;
 	}
 
+	// Read the headers of all files.
 	for (i = 1; i < argc; i++)
 	{
 		if (!strncmp(argv[i], "--", 2))
 		{
-			printf("bla\n");
+			i++;
 			continue;
 		}
 
-		vec = &vecs[i];
+		vec = &vecs[vec_index];
+		vec_index++;
 		vec->filename = argv[i];
 
-		if (!(vec->input = fopen(argv[1], "rb")))
+		if (!(vec->input = fopen(vec->filename, "rb")))
 		{
-			fprintf(stderr, "Failed to open file %s\n", argv[1]);
+			fprintf(stderr, "Failed to open file %s\n", vec->filename);
 			ret = -1;
 			goto fail;
 		}
 
 		fread(&vec->count, sizeof(vec->count), 1, vec->input);
 		fread(&vec->vecsize, sizeof(vec->vecsize), 1, vec->input);
-		// TODO: Read 2 more shorts (What are they)
+		fread(&min, sizeof(min), 1, vec->input);
+		fread(&max, sizeof(max), 1, vec->input);
 
-		printf("%s: Contains %d vectors of size %d\n",
-			vec->filename, vec->count, vec->vecsize);
+		if (!vecsize)
+		{
+			vecsize = vec->vecsize;
+		}
+		else if (vecsize != vec->vecsize)
+		{
+			fprintf(stderr, "Error! Size %d for %s is not the same as the expected size %d. "
+				"All .vec files must contain samples of the same size!\n",
+				vec->vecsize, vec->filename, vecsize);
+			ret = -1;
+			goto fail;
+		}
+
+		printf("%s: Contains %d vectors of size %d. Min %hd, Max %hd\n",
+			vec->filename, vec->count, vec->vecsize, min, max);
 
 		mergevec.count += vec->count;
+	}
+
+	if (merge)
+	{
+		int sample_size = (vecsize * sizeof(short)) + sizeof(unsigned char);
+
+		printf("Merging files...\n");
+
+		if (!(output = fopen(merge, "w")))
+		{
+			fprintf(stderr, "Failed to open merge file %s for writing: %s\n",
+				merge, strerror(errno));
+			ret = -1;
+			goto fail;
+		}
+
+		if (write_vec_header(output, mergevec.count, vecsize, 0, 0))
+		{
+			ret = -1;
+			goto fail;
+		}
+
+		for (i = 0; i < file_count; i++)
+		{
+			vec = &vecs[i];
+
+			if (!(samples = (short *)malloc(sample_size * vec->count)))
+			{
+				fprintf(stderr, "Out of memory!\n");
+				ret = -1;
+				goto fail;
+			}
+
+			fread(samples, sample_size, vec->count, vec->input);
+			fwrite(samples, sample_size, vec->count, output);
+
+			free(samples);
+			samples = NULL;
+		}
+
+		printf("Successfully merged %d vectors into %s\n", mergevec.count, merge);
 	}
 
 fail:
 	if (vecs)
 	{
-		for (i = 0; i < argc; i++)
+		for (i = 0; i < file_count; i++)
 		{
 			if (vecs[i].input)
 			{
 				fclose(vecs[i].input);
 			}
+
+			if (vecs[i].vector)
+			{
+				free(vecs[i].vector);
+				vecs[i].vector = NULL;
+			}
 		}
 
 		free(vecs);
+	}
+
+	if (output)
+	{
+		fclose(output);
+	}
+
+	if (samples)
+	{
+		free(samples);
 	}
 
 	return ret;
